de07d636c799057130f3d425b74213f3871bad16
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-11-05 10:39:49 +0100

    removed unnnecessary module imports

--- songcrud/gbedu/views.py
+++ songcrud/gbedu/views.py
@@ -1,7 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import serializers, status
 from musicapp.models import Artiste, Song, Lyric
 from gbedu.serializers import ArtisteSerializer, SongSerializer, LyricSerializer
 
