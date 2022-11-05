00e5d7caf290c106eb15fb65e55fbb8adc40cf66
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-11-05 10:24:00 +0100

    routers enabled

--- songcrud/gbedu/views.py
+++ songcrud/gbedu/views.py
@@ -1,4 +1,4 @@
-# from rest_framework import viewsets
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import serializers, status
@@ -6,75 +6,75 @@ from musicapp.models import Artiste, Song, Lyric
 from gbedu.serializers import ArtisteSerializer, SongSerializer, LyricSerializer
 
 
-@api_view(['GET'])
-def gbeduOverview(request):
-    api_urls = {
-        'all_artistes': '/',
-        'Search by Artiste': '/?category=category_name',
-        'Search by Song': '/?subcategory=category_name',
-        'Add': '/create',
-        'Update': '/update/pk',
-        'Delete': '/artiste/pk/delete'
-    }
+# @api_view(['GET'])
+# def gbeduOverview(request):
+#     api_urls = {
+#         'all_artistes': '/',
+#         'Search by Artiste': '/?category=category_name',
+#         'Search by Song': '/?subcategory=category_name',
+#         'Add': '/create',
+#         'Update': '/update/pk',
+#         'Delete': '/artiste/pk/delete'
+#     }
   
-    return Response(api_urls)
+#     return Response(api_urls)
 
 
-@api_view(['POST'])
-def add_artistes(request):
-    artiste = ArtisteSerializer(data=request.data)
+# @api_view(['POST'])
+# def add_artistes(request):
+#     artiste = ArtisteSerializer(data=request.data)
   
-    # validating for already existing data
-    if Artiste.objects.filter(**request.data).exists():
-        raise serializers.ValidationError('This data already exists')
+#     # validating for already existing data
+#     if Artiste.objects.filter(**request.data).exists():
+#         raise serializers.ValidationError('This data already exists')
   
-    if artiste.is_valid():
-        artiste.save()
-        return Response(artiste.data)
-    else:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+#     if artiste.is_valid():
+#         artiste.save()
+#         return Response(artiste.data)
+#     else:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 
-@api_view(['GET'])
-def view_artistes(request):
+# @api_view(['GET'])
+# def view_artistes(request):
     
-   # checking for the parameters from the URL
-   if request.query_params:
-      artistes = Artiste.objects.filter(**request.query_param.dict())
-   else:
-      artistes = Artiste.objects.all()
-   serializer = ArtisteSerializer(artistes, many=True)
+#    # checking for the parameters from the URL
+#    if request.query_params:
+#       artistes = Artiste.objects.filter(**request.query_param.dict())
+#    else:
+#       artistes = Artiste.objects.all()
+#    serializer = ArtisteSerializer(artistes, many=True)
   
-   # if there is something in artistes else raise error
-   if artistes:
-      return Response(serializer.data)
-   else:
-      return Response(status=status.HTTP_404_NOT_FOUND)
+#    # if there is something in artistes else raise error
+#    if artistes:
+#       return Response(serializer.data)
+#    else:
+#       return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 
-@api_view(['POST'])
-def update_artistes(request, pk):
-   artiste = Artiste.objects.get(pk=pk)
-   data = ArtisteSerializer(instance=artiste, data=request.data)
+# @api_view(['POST'])
+# def update_artistes(request, pk):
+#    artiste = Artiste.objects.get(pk=pk)
+#    data = ArtisteSerializer(instance=artiste, data=request.data)
   
-   if data.is_valid():
-      data.save()
-      return Response(data.data)
-   else:
-      return Response(status=status.HTTP_404_NOT_FOUND)
+#    if data.is_valid():
+#       data.save()
+#       return Response(data.data)
+#    else:
+#       return Response(status=status.HTTP_404_NOT_FOUND)
 
-# class ArtisteViewSet(viewsets.ModelViewSet):
-#    queryset = Artiste.objects.all()
-#    serializer_class = ArtisteSerializer
-
-
-# class SongViewSet(viewsets.ModelViewSet):
-#    queryset = Song.objects.all()
-#    serializer_class = SongSerializer
+class ArtisteViewSet(viewsets.ModelViewSet):
+   queryset = Artiste.objects.all()
+   serializer_class = ArtisteSerializer
 
 
-# class LyricViewSet(viewsets.ModelViewSet):
-#    queryset = Lyric.objects.all()
-#    serializer_class = LyricSerializer
+class SongViewSet(viewsets.ModelViewSet):
+   queryset = Song.objects.all()
+   serializer_class = SongSerializer
+
+
+class LyricViewSet(viewsets.ModelViewSet):
+   queryset = Lyric.objects.all()
+   serializer_class = LyricSerializer
