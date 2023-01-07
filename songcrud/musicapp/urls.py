d7714bfbb765f3354ed045b2961dcd20cae4cec9
MrDipo <42739211+MrDipo@users.noreply.github.com>
2023-01-08 00:20:16 +0100

    routed all url paths to appropriate pages

--- songcrud/musicapp/urls.py
+++ songcrud/musicapp/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('create-artiste', views.create_artiste, name="create"),
+    path('show-artistes', views.show_artistes, name="show"),
+    path('edit-artiste/<int:id>', views.edit_artistes, name="edit"),
+    path('update-artiste/<int:id>', views.update_artiste, name="update"),
+    path('delete-artiste/<int:id>', views.destroy_artiste, name="delete"),
 ]
